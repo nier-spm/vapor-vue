6a48eab2c8b8582b6cdf8ebc2119f5c695c0a33d
Nier Liu <curse0122@gmail.com>
2021-07-23 21:14:46 +0800

    update gitignore

--- Sources/vapor-vue/vapor_vue.swift
+++
@@ -1,3 +0,0 @@
-struct vapor_vue {
-    var text = "Hello, World!"
-}
--- Tests/VaporVueTests/VaporVueTests.swift
+++ Tests/VaporVueTests/VaporVueTests.swift
@@ -1,11 +1,10 @@
     import XCTest
     @testable import vapor_vue
 
-    final class vapor_vueTests: XCTestCase {
+    final class VaporVueTests: XCTestCase {
         func testExample() {
             // This is an example of a functional test case.
             // Use XCTAssert and related functions to verify your tests produce the correct
             // results.
-            XCTAssertEqual(vapor_vue().text, "Hello, World!")
         }
     }
